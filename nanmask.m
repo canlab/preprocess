d6e496c95bdee847c28d45d875472655b983e68a
Dan Weflen <danweflen@gmail.com>
2017-02-17 16:43:31 -0700

    Reformatted nanmask.m for readability.

--- nanmask.m
+++ nanmask.m
@@ -9,48 +9,35 @@ function out = nanmask(P,varargin)
 % including only voxels in the output image where the mask image values are
 % greater than zero.
 % an empty 2nd argument calls the spm gui to ask for a mask image
-if length(varargin) > 0, 
+  if length(varargin) > 0, 
     maskP = varargin{1}; ,
     if isempty(maskP)
-        maskP = spm_get(1,'*img','Select mask image');
+      maskP = spm_get(1,'*img','Select mask image');
     end
-end
-
-if isempty(P)
+  end
+  if isempty(P)
     P = spm_get(Inf,'*img','Select images to mask');
-end
-
-for i = 1:size(P,1)
-    
-    % f is input, Q is output file name
+  end
+  for i = 1:size(P,1)
+  % f is input, Q is output file name
     f = deblank(P(i,:));
-
     [d,fn,e] = fileparts(f);
     Q = fullfile(d,['m' fn e]);
-    
     if exist('maskP') == 1
-        
-        f = str2mat(f,maskP);
-        
-        % apply brain mask
-        spm_imcalc_ui(f,Q,'i1 .* (i2 > 0)',{0 1 spm_type('float') 0});
-
-        % turn zeros into NaNs
-        spm_imcalc_ui(Q,Q,'i1 + 0 ./ i1',{0 1 spm_type('float') 0});
-    
+      f = str2mat(f,maskP);
+      % apply brain mask
+      spm_imcalc_ui(f,Q,'i1 .* (i2 > 0)',{0 1 spm_type('float') 0});
+      % turn zeros into NaNs
+      spm_imcalc_ui(Q,Q,'i1 + 0 ./ i1',{0 1 spm_type('float') 0});
     else
- 
-        % turn zeros into NaNs
-        spm_imcalc_ui(f,Q,'i1 + 0 ./ i1',{0 1 spm_type('float') 0});
-        
+    % turn zeros into NaNs
+      spm_imcalc_ui(f,Q,'i1 + 0 ./ i1',{0 1 spm_type('float') 0});
     end
-        
     if i == 1, 
-        out = Q;
+      out = Q;
     else
-        out = str2mat(out,Q);
+      out = str2mat(out,Q);
     end
-    
+  end
+  return
 end
-return
-
